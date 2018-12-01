2e186361c001229ae63b6ee9183bf7c4ef3bdc41
Rafał Zubala <rzubala@operatus.org>
2018-12-01 19:17:34 +0100

    01 - finished

--- 01/solve.py
+++ 01/solve.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python -tt
-# -*- coding: utf-8 -*-
 
 import sys
-import re
 
 def calc(filename):
   sum = 0
+  intersum = []
+  freqs = []  
+  rep = False  
   with open(filename) as file:
     for line in file:
-      #print line,
-      sum += int(line)
+      diff = int(line)
+      sum += diff
+      freqs.append(diff)
+      if sum in intersum:
+        print 'repetition: ' + str(sum)
+        rep = True
+      intersum.append(sum)
     print 'sum:', sum
+   
+  while not rep:
+    for f in freqs:
+      sum += f
+      if sum in intersum:
+        print 'repetition: ' + str(sum)
+        rep = True
+        break
 
 def main():
   args = sys.argv[1:]
