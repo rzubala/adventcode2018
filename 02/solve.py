9ce1100a589045907d22b3d3349140c2147ad0ee
Rafał Zubala <rzubala@operatus.org>
2018-12-02 09:07:48 +0100

    02 - part 2

--- 02/solve.py
+++ 02/solve.py
@@ -20,17 +20,41 @@ def countChar(txt):
   res = {"2" : two,"3" : three}
   return res
 
+def compare(txt1, txt2):
+  a1 = list(txt1)
+  a2 = list(txt2)
+  cnt = 0
+  for i in range(len(a1)):
+    if a1[i] != a2[i]:
+      cnt += 1;
+    if cnt > 2:
+      return False
+  return cnt == 1
+
+def findClose(lines):
+  for t1 in lines:
+    for t2 in lines:
+      if t1 != t2:
+        if compare(t1, t2):
+          print t1
+          print t2
+          return
+
 def calc(filename):
   two = 0
   three = 0
+  lines = []  
   with open(filename) as file:
     for line in file:
       txt = line.rstrip('\n')
+      lines.append(txt)
       stat = countChar(txt)
       two += stat.get("2", 0)
       three += stat.get("3", 0)
   print 'found: ', two, three
   print 'res: ', two * three  
+
+  findClose(lines)
 
 def main():
   args = sys.argv[1:]
