83d5d86463a4115962872054ad17b56083ce5f16
Rafał Zubala <rzubala@operatus.org>
2018-12-10 20:42:03 +0100

    08 - part 2

--- 08/solve.py
+++ 08/solve.py
@@ -8,22 +8,34 @@ def read(filename):
       return line.split()
       
 def calc(data, metadata):
- nodes = int(data[0])
- meta = int(data[1])
- pos = 2   
- if nodes:   
-   for n in range(nodes):
-     pos += calc(data[pos:], metadata)
- for m in range(meta):   
-   metadata.append(int(data[pos]))
-   pos += 1
- return pos   
+  nodes = int(data[0])
+  meta = int(data[1])
+  pos = 2   
+  nvalue = []   
+  if nodes:   
+    for n in range(nodes):
+      res = calc(data[pos:], metadata)
+      pos += res[0]
+      nvalue.append(res[1])
+
+  summ = 0
+  for m in range(meta):  
+    val = int(data[pos])  
+    metadata.append(val)
+    pos += 1
+    if not nodes: 
+      summ += val
+    else:
+      val -= 1
+      if val < len(nvalue):
+        summ += nvalue[val] 
+  return (pos, summ)   
 
 def parse(filename):
   data = read(filename)
   metadata = []  
-  calc(data, metadata)
-  print 'meta', sum(metadata)  
+  res = calc(data, metadata)
+  print 'meta', sum(metadata), res[1]  
 
 def main():
   args = sys.argv[1:]
