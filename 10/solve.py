a6b733e139207abd05421c9f89acc1f998d004d9
Rafał Zubala <rzubala@operatus.org>
2018-12-11 10:04:11 +0100

    10 - part1 and part2

--- 10/solve.py
+++ 10/solve.py
@@ -17,11 +17,11 @@ def parse(line):
     print 'Parse error', line,  
     return (0,0,0,0,0)
 
-def getSize(points):
-  min_x = min(points)[0]
-  max_x = max(points)[0]  
-  min_y = min(points, key=lambda item:item[1])[1]
-  max_y = max(points, key=lambda item:item[1])[1]  
+def getSize(points, t):
+  min_x = min(x + t * vx for (x, y, vx, vy) in points)
+  max_x = max(x + t * vx for (x, y, vx, vy) in points)
+  min_y = min(y + t * vy for (x, y, vx, vy) in points)
+  max_y = max(y + t * vy for (x, y, vx, vy) in points)
   return (min_x, max_x, min_y, max_y)
 
 def isPoint(x, y, points, time):
@@ -31,12 +31,28 @@ def isPoint(x, y, points, time):
   return False  
 
 def printPoints(points, time):
-  size = getSize(points) 
+  size = getSize(points, time) 
   for y in range(size[2], size[3]+1):
     for x in range(size[0], size[1]+1):
       sys.stdout.write('#') if isPoint(x, y, points, time) else sys.stdout.write('.')
     print    
   print
+      
+def findMinSize(points):       
+  min_time = None
+  min_size = None
+  for t in range(30000):
+    size = getSize(points, t)  
+    min_x = size[0]
+    max_x = size[1]
+    min_y = size[2]
+    max_y = size[3]
+    size = max_x - min_x + max_y - min_y
+    if not min_size or min_size > size:
+      min_size = size
+      min_time = t
+  print 'min time', min_time, min_size  
+  return min_time  
 
 def calc(filename):
   points = []  
@@ -44,8 +60,8 @@ def calc(filename):
     for line in file:
       p = parse(line)
       points.append(p)
-  for t in range (0,5):  
-    printPoints(points, t)  
+  time = findMinSize(points)  
+  printPoints(points, time)
 
 def main():
   args = sys.argv[1:]
