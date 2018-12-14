f8300b88aa7d912c8e03e904852158b5f9ff23ea
Rafał Zubala <rzubala@operatus.org>
2018-12-14 09:02:54 +0100

    14 - part2 - initial

--- 14/solve.py
+++ 14/solve.py
@@ -18,6 +18,8 @@ def calc(c, x, y):
   r = [x, y]
   e1 = 0
   e2 = 1  
+  cn = int(c) 
+  prev = 0  
   while True:
     v = list(str(r[e1] + r[e2]))
     v = [int(x) for x in v]
@@ -28,15 +30,33 @@ def calc(c, x, y):
     e1 = (e1 + r[e1] + 1) % l
     e2 = (e2 + r[e2] + 1) % l
     
-    if l >= c + 10:
-      print ''.join([str(x) for x in r[c:c+10]])
+    #print r, prev
+    #print r[prev:]
+
+    res = ''.join([str(x) for x in r[prev:]]).find(c)
+    if res > 0:
+      print 'res:', res+prev  
       break
+    prev = l - 6
+    if prev < 0:
+      prev = 0
+
+    if l % 100000 == 0:  
+      print 'len: ', l
+      
+    #part1
+    #if l >= cn + 10:
+    #  print ''.join([str(x) for x in r[cn:cn+10]])
+    #  break
 
 def main():
   args = sys.argv[1:]
 
-  #calc(2018, 3 ,7)
-  calc(74501, 3 ,7)
+  #calc('74501', 3 ,7)
+  #calc('9', 3 ,7)
+  #calc('51589', 3 ,7)
+  calc('074501', 3 ,7)
+  #calc('59414', 3 ,7)
   
 if __name__ == '__main__':
   main()
