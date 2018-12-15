e6a916c66fe7e9b13288e947451733d827485ae9
Rafał Zubala <rzubala@operatus.org>
2018-12-15 17:36:10 +0100

    15 - fill distances

--- 15/solve.py
+++ 15/solve.py
@@ -13,71 +13,75 @@ def findElement(points, elem):
   y = 0 
   res = []  
   for line in points:
-    lineStr = ''.join(line)
-    res += [(i,y) for i, s in enumerate(lineStr) if s == elem]
+    x = 0
+    for c in line:
+      if c == elem:
+        res += [(x, y)]
+      x += 1
     y += 1
   return res
 
-def getNextMoves(points, p, toP):
+def getNextMove(points, p):
   res = []
   x = p[0]
   y = p[1]
-  dx = toP[0]  
-  dy = toP[1]  
   if x > 0:
     xn = x - 1
-    if points[y][xn] == '.' or xn == dx and y == dy:
-      res.append((xn,y))
+    res.append((xn,y))
   if x < len(points[y]) - 1:
     xn = x + 1  
-    if points[y][xn] == '.' or xn == dx and y == dy:
-      res.append((xn,y))
+    res.append((xn,y))
   if y > 0:
     yn = y - 1
-    if points[yn][x] == '.' or x == dx and yn == dy:
-      res.append((x, yn))
+    res.append((x, yn))
   if y < len(points) - 1:
     yn = y + 1  
-    if points[yn][x] == '.' or x == dx and yn == dy:
-      res.append((x, yn))
-
+    res.append((x, yn))
+  
   return res  
 
-def findPath(points, fromP, toP, visited, cnt):
+def fillDistances(points, cnt):
+  moves = findElement(points, str(cnt))
+  if not moves:
+    print 'not found', cnt  
+    return
+  for m in moves:  
+    nextMoves = getNextMove(points, m)
+    nextMoves = [p for p in nextMoves if '.' == points[p[1]][p[0]]] 
+    for n in nextMoves:
+      x = n[0]
+      y = n[1]
+      points[y][x] = str(cnt+1)
+  fillDistances(points, cnt+1)
 
-  if cnt > 50:
-    return None
-    
-  print fromP, toP, cnt
+def getDistances(points, fromP, cnt):
+  nextMoves = getNextMove(points, fromP)
+  nextMoves = [p for p in nextMoves if '.' == points[p[1]][p[0]]] 
 
-  visited.append(fromP)  
-  nextMoves = getNextMoves(points, fromP, toP) 
-  if toP in nextMoves:
-    #print 'found', toP
-    return cnt
-  paths = []  
   for n in nextMoves:
-    if n not in visited:
-      #print 'goto', n
-      path = findPath(points, n, toP, visited[:], cnt+1)
-      if path:
-        paths.append(path)
-  if paths:
-    return min(paths)
-  return None
+    x = n[0]
+    y = n[1]
+    points[y][x] = str(cnt)
+
+  fillDistances(points, cnt)
+  printPoints(points)  
+
+def printPoints(points):    
+  for y in points:
+    for x in y:
+      sys.stdout.write(x)
+    print
+  print  
 
 def calc(filename):
   points = parse(filename)
 
   elfs = findElement(points, 'E')
   gobs = findElement(points, 'G')
-    
-  print findPath(points, elfs[0], gobs[0], [], 0)
 
-  for y in points:
-    for x in y:
-      sys.stdout.write(x)
-    print
+  getDistances([row[:] for row in points], gobs[0], 1)
+    
+  printPoints(points)  
 
 def main():
   args = sys.argv[1:]
