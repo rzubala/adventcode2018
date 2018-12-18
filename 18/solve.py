0c8579cb26f019cdc1d6a7f91c7d5cce340e7276
Rafał Zubala <rzubala@operatus.org>
2018-12-18 07:03:15 +0100

    18 - part1

--- 18/solve.py
+++ 18/solve.py
@@ -15,9 +15,66 @@ def printRes(res):
       sys.stdout.write(x)
     print
 
+def getVal(res, y, x):
+  if x < 0 or y < 0:
+    return None
+  if x >= len(res) or y >= len(res):  
+    return None
+  try:
+    return res[y][x]
+  except:
+    return None
+
+def getNext(prev,res, y, x):
+  neighs = []  
+  for yn in range(y-1, y+2):
+    for xn in range(x-1, x+2):
+      if x == xn and y == yn:
+        continue
+      point = getVal(prev, yn, xn)
+      if point:
+        neighs.append(point)  
+
+  if res[y][x] == '.' and len([n for n in neighs if n == '|']) > 2:
+    res[y][x] = '|'
+  elif res[y][x] == '|' and len([n for n in neighs if n == '#']) > 2:
+    res[y][x] = '#'
+  elif res[y][x] == '#':
+    if len([n for n in neighs if n == '#']) > 0 and len([n for n in neighs if n == '|']) > 0:
+      res[y][x] = '#'
+    else:  
+      res[y][x] = '.'
+
+
+def convert(res, m):
+
+  prev = [row[:] for row in res]  
+  y = 0  
+  for line in res:
+    x = 0
+    for c in line:
+      cn = getNext(prev,res,y,x)
+      x += 1
+    y += 1
+
+  print '\nAfter', m   
+  printRes(res)  
+
 def calc(filename):
   res = parse(filename)
-  printRes(res)  
+
+  printRes(res) 
+  for i in range (1, 11):  
+    convert(res, i) 
+
+  cntl = 0
+  cntt = 0
+  for line in res:
+    cntt += len([n for n in line if n == '|'])
+    cntl += len([n for n in line if n == '#'])
+
+  print 'res', cntl, cntt, cntl * cntt  
+
 
 def main():
   args = sys.argv[1:]
