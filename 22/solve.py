16a285c7dcb4929641eb05c074011477ca172721
Rafał Zubala <rzubala@operatus.org>
2018-12-22 15:59:59 +0100

    22 - part2 initial

--- 22/solve.py
+++ 22/solve.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python -tt
 
 import sys
+import networkx
+
+# 0 = nothing, 1 = climbing gear, 2 = torch
+def getTools(e):
+    if e == 0:        # rocky
+      return {1, 2}          # gear, torch
+    if e == 1:        # wet
+      return {0, 1}       # nothing, gear
+    if e == 2:        # narrow
+      return {0, 2}       # torch, nothing
 
 def calc(depth, t):
   els = {} 
-  rl = 0
-  for y in range(0,t[1]+1):
-    for x in range(0,t[0]+1):
+  cave = {}
+
+  for y in range(0,t[1]+1000):
+    for x in range(0,t[0]+1000):
       if (x,y) == (0,0) or (x,y) == t:  
         gi = 0
       elif y == 0:
@@ -19,22 +30,23 @@ def calc(depth, t):
       el = (gi + depth) % 20183
       lastel = el
       els[x] = el
-
-      if (x,y) == (0,0):
-        el = 'M'  
-      elif (x,y) == t:
-        el = 'T'
-      else:  
-        if el % 3 == 0:
-          el = '.'
-          rl += 0
-        elif el % 3 == 1:
-          el = '='
-          rl += 1
-        elif el % 3 == 2:
-          el = '|'
-          rl += 2
+      cave[(x,y)] = el % 3
+  
+  rl = 0
+  for y in range(0,t[1]+1):
+    for x in range(0,t[0]+1):
+      rl += cave[(x,y)]
   print 'risk level', rl
+
+  G = networkx.DiGraph()    #directional graph
+  for y in range(0,t[1]+1000):
+    for x in range(0,t[0]+1000):
+      tools = getTools(cave[(x,y)])
+      for tool1 in tools:
+        for tool2 in tools:
+          if tool1 != tool2:
+            G.add_edge((x, y, tool1), (x, y, tool2), weight=7)
+
 
 def main():
   
