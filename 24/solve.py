99bee4242526f4a3e21a81e09f89c4177446e9f5
Rafał Zubala <rzubala@operatus.org>
2018-12-24 11:38:39 +0100

    24 - group parsing

--- 24/solve.py
+++ 24/solve.py
@@ -8,7 +8,7 @@ def parse(filename):
   infection = False
   with open(filename) as file:
     for line in file:
-      print line.rstrip('\n')
+      #print line.rstrip('\n')
 
       s = re.search(r'Infection', line)
       if s:
@@ -17,7 +17,7 @@ def parse(filename):
 
       s = re.search(r'(\d+) units .+ with (\d+) hit points (\((.+)\))? ?with an attack that does (\d+) (.+) damage at initiative (\d+)', line)  
       if s:
-        units = int(s.group(1))
+        groups = int(s.group(1))
         hit_points = int(s.group(2))
         weak_immune = s.group(4)
         damage = int(s.group(5))
@@ -36,12 +36,22 @@ def parse(filename):
           if si:
             immunes = si.group(1).split(',')
 
-        print infection, units, hit_points, weaks, immunes, damage, damage_type, initiative
+        #print infection, groups, hit_points, weaks, immunes, damage, damage_type, initiative
+        res.append(Group(infection, groups, hit_points, immunes, weaks, initiative, damage, damage_type))
         
   return res
 
 def calc(filename):
   res = parse(filename)
+  fight(res)
+
+def fight(groups_initial):
+  #print groups_initial  
+  while True:
+    groups = sorted(groups_initial, key=lambda g: (g.getEffectivePower(), g.initiative), reverse=True)
+    for g in groups:
+      print g.getEffectivePower(), g.initiative, g.infection    
+    break 
 
 def main():
   args = sys.argv[1:]
@@ -51,6 +61,29 @@ def main():
     sys.exit(1)
   
   calc(args[0])
+
+class Group(object):
+  def __init__(self, infection, groups, hit_points, immunes, weaks, initiative, damage, damage_type):
+    self.infection = infection
+    self.groups = groups
+    self.hit_points = hit_points
+    self.immunes = immunes
+    self.weaks = weaks
+    self.damage = damage
+    self.damage_type = damage_type
+    self.target = None
+    self.initiative = initiative,
+
+  def getEffectivePower(self):
+    return self.groups * self.damage
+
+  def getDamage(self, enemy):
+    if self.damage_type in enemy.immunes:
+      return 0
+    elif self.damage_type in enemy.weaks:
+      return self.getEffectivePower() * 2
+    else:
+      return self.getEffectivePower()
   
 if __name__ == '__main__':
   main()
