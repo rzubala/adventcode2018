13e78ce8020e0a4c2948857e00d6ffc4fcadaedd
Rafał Zubala <rzubala@operatus.org>
2018-12-24 22:15:50 +0100

    24

--- 24/solve.py
+++ 24/solve.py
@@ -6,6 +6,7 @@ import re
 def parse(filename):
   res = []
   infection = False
+  id = 0  
   with open(filename) as file:
     for line in file:
       #print line.rstrip('\n')
@@ -37,21 +38,51 @@ def parse(filename):
             immunes = si.group(1).split(',')
 
         #print infection, groups, hit_points, weaks, immunes, damage, damage_type, initiative
-        res.append(Group(infection, groups, hit_points, immunes, weaks, initiative, damage, damage_type))
+        res.append(Group(id, infection, groups, hit_points, immunes, weaks, initiative, damage, damage_type))
+        id += 1
         
+        print id, infection, groups, hit_points, immunes, weaks, initiative, damage, damage_type
+
   return res
 
 def calc(filename):
   res = parse(filename)
-  fight(res)
+  print fight(res)
 
 def fight(groups_initial):
-  #print groups_initial  
   while True:
     groups = sorted(groups_initial, key=lambda g: (g.getEffectivePower(), g.initiative), reverse=True)
+    selected = set()
     for g in groups:
-      print g.getEffectivePower(), g.initiative, g.infection    
-    break 
+      targets = sorted([en for en in groups if en.infection != g.infection and en.id not in selected and g.getDamage(en)>0], key=lambda x: (g.getDamage(x), x.getEffectivePower(), x.initiative), reverse=True)
+      if targets:
+        g.target = targets[0]
+        selected.add(targets[0].id)
+        
+    groups = sorted(groups, key=lambda x: x.initiative, reverse=True)
+    any_killed = False
+    for g in groups:
+      if g.target:
+        damage = g.getDamage(g.target)
+        destroyed = min(g.target.units, damage/g.target.hit_points)
+        if destroyed > 0:
+          any_killed = True
+        g.target.units -= destroyed
+        
+    groups = [g for g in groups if g.units > 0]
+    for g in groups:
+      g.target = None
+
+    if not any_killed:
+      return 1, imm
+
+    inf = sum([g.units for g in groups if g.infection])
+    imm = sum([g.units for g in groups if not g.infection])
+    
+    if inf == 0:
+      return 1, imm
+    if imm == 0:
+      return 0, inf
 
 def main():
   args = sys.argv[1:]
@@ -63,19 +94,20 @@ def main():
   calc(args[0])
 
 class Group(object):
-  def __init__(self, infection, groups, hit_points, immunes, weaks, initiative, damage, damage_type):
+  def __init__(self, id, infection, units, hit_points, immunes, weaks, initiative, damage, damage_type):
     self.infection = infection
-    self.groups = groups
+    self.units = units
     self.hit_points = hit_points
     self.immunes = immunes
     self.weaks = weaks
     self.damage = damage
     self.damage_type = damage_type
     self.target = None
-    self.initiative = initiative,
+    self.initiative = initiative
+    self.id = id
 
   def getEffectivePower(self):
-    return self.groups * self.damage
+    return self.units * self.damage
 
   def getDamage(self, enemy):
     if self.damage_type in enemy.immunes:
